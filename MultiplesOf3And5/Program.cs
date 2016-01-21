c37e6c98b2e956c879034c98f44116dc8b9f016e
jakepens71 <jake_ps@comcast.net>
2016-01-21 09:12:05 -0500

    works and finished using linq

---
+++ MultiplesOf3And5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            int maxNumber = 1000;
+
+            for (int i = 0; i < maxNumber; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            for (int y = 0; y < maxNumber; y++)
+            {
+                if (y % 5 == 0)
+                {
+                    numbers.Add(y);
+                }
+            }
+
+            int total = (from number in numbers where number < maxNumber select number).Distinct().Sum();
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
